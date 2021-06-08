abce27967782e2ca32e2d6020c12ec7d35b68e95
Mandar Parte <mandarparte1709@gmail.com>
2021-06-08 20:35:39 +0530

    Testing with input blocks!

--- testInput.py
+++ testInput.py
@@ -5,7 +5,7 @@ required = ["Gym", "School", "Store"]
 inputList = [
     {required[0]: True, required[1]: True, required[2]: False},
     {required[0]: False, required[1]: False, required[2]: False},
-    {required[0]: False, required[1]: False, required[2]: False},
+    {required[0]: False, required[1]: False, required[2]: True},
     {required[0]: False, required[1]: True, required[2]: False},
     {required[0]: False, required[1]: True, required[2]: False}
 ]
